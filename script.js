cbafe2791f3fe45bcfc9bf1707c87a8984aca762
Luka Talakvadze <lukatalakvadze@gmail.com>
2024-07-22 19:54:31 +0400

    style: add footer section with animations

--- script.js
+++ script.js
@@ -6,12 +6,18 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-document.addEventListener("DOMContentLoaded", () => {
-  const galleryContainer = document.querySelector(".gallery-container");
+const titleAndArrowDivs = document.querySelectorAll(".title-and-arrow-div");
 
-  // Example: Auto-scroll to the right on load
-  galleryContainer.scrollTo({
-    left: galleryContainer.scrollWidth,
-    behavior: "smooth",
+titleAndArrowDivs.forEach((div) => {
+  div.addEventListener("click", (event) => {
+    const footerBox = event.currentTarget.closest(".footer-box");
+
+    document.querySelectorAll(".footer-box").forEach((box) => {
+      if (box !== footerBox) {
+        box.classList.remove("active");
+      }
+    });
+
+    footerBox.classList.toggle("active");
   });
 });
